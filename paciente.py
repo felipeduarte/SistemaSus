423d2a38f9f4b67451159c7e01162b7ab4f34ba1
Felipe Duarte <felipe.duarte55@gmail.com>
2013-11-15 21:08:51 -0200

    refatoracao do codigo

--- paciente.py
+++ paciente.py
@@ -1,19 +1,20 @@
-from datetime import date
+from time import localtime
 
 class Paciente(object):
     
-    def __init__(self, nome, sexo, data_de_nascimento, codigo_do_seguro_social):
+    def __init__(self, nome, sexo, ano_de_nascimento, codigo_do_seguro_social):
         self.nome = nome
         self.sexo = sexo
-        self.data_de_nascimento = data_de_nascimento
+        self.ano_de_nascimento = ano_de_nascimento
         self.codigo_do_seguro_social = codigo_do_seguro_social
         self.funcionarios = []
                 
     def calcular_idade(self):
-        hoje = date.today()
-        data_de_aniversario = date(int(self.data_de_nascimento[6:]), int(self.data_de_nascimento[3:5]), int(self.data_de_nascimento[:2]))
-        idade = (hoje - data_de_aniversario)
-        return int((idade.days)/365)
+        idade = 0
+        data_completa = localtime()
+        ano_atual = data_completa[0]
+        idade = ano_atual - self.ano_de_nascimento
+        return idade
 
     def vincular_funcionario(self, objeto_funcionario):
         self.funcionarios.append(objeto_funcionario)
